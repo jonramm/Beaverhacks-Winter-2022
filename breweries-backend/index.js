783915697d8098c4525126653a5f0d9fc8234175
Jon Ramm <jrgbone@gmail.com>
2022-01-05 19:54:56 -0700

    add post route handler for brewery data retrieval

--- breweries-backend/index.js
+++ breweries-backend/index.js
@@ -1,12 +1,33 @@
 // Dependencies
 import express from 'express';
+import https from 'https';
+import bodyParser from 'body-parser';
 
 // Declare instance of express 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Connect to database through mongoose
 // Skipping for now 
 
+
+// Connect to OpenBreweryDB
+app.post("/", (req, res)=> {
+  const query = req.body.city;
+  const url = `https://api.openbrewerydb.org/breweries?per_page=50&by_city=${query}`
+  // code below written wit the help of StackExchange
+  https.get(url, (response)=> {
+    const chunks = [];
+    response.on("data", (chunk)=> {
+      chunks.push(chunk);
+    })
+    response.on("end", ()=> {
+      const breweryData = JSON.parse(chunks.join(''));
+      res.status(200).json(breweryData);
+    })
+  })
+})
 
 // Start Server 
 app.listen(3000, () => {
