8651585d91511fd70de9ecf74153c4c28ae77b28
Jon Ramm <jrgbone@gmail.com>
2022-01-05 20:42:26 -0700

    add components folder

--- brewery-app/src/App.js
+++ brewery-app/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  
+  // testing api call
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(()=> {
+    fetch("/api")
+      .then((res)=> res.json())
+      .then((data)=> setData(data));
+  }, []);
+  //
+  
+
   return (
     <div>
       <h1>Hello!</h1>
       <p>This will one day contain glorious brewery data</p>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
