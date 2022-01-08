5a59bde0c18039f98df90bd84503a203f966ad09
Jon Ramm <jrgbone@gmail.com>
2022-01-08 10:58:32 -0700

    home page looks better

--- brewery-app/src/components/pageNavbar.js
+++ brewery-app/src/components/pageNavbar.js
@@ -8,19 +8,13 @@ function pageNavbar() {
     return (
         <Navbar bg="primary" expand="lg" variant="dark">
             <Container fluid>
-                <Navbar.Brand href="/">Brewery Finder</Navbar.Brand>
+                <Navbar.Brand href="/">HopHub</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" />
                 <Navbar.Collapse id="navbarColor01">
                     <Nav className="me-auto">
                         <Nav.Link href="/" active>Home</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/breweries">Search By City</NavDropdown.Item>
-                            <div class="dropdown-divider"></div>
-                            <NavDropdown.Item href="/about">About</NavDropdown.Item>
-                            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-                            {/* <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-                        </NavDropdown>
+                        <Nav.Link href="/breweries">Search By City</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- brewery-app/src/pages/Home.js
+++ brewery-app/src/pages/Home.js
@@ -5,68 +5,60 @@ import Navbar from '../components/pageNavbar';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios'
 import BreweryList from '../components/BreweryList';
+import icon from '../images/hophub-icon.png'
 
 function Home() {
 
-    const key = "AIzaSyCGyHyprwTjmZ-cENr1NZDn8AEKHqpU3oY"
-
-    //creating IP state
+    //creating IP, coordinate, and breweries state
     const [ip, setIP] = useState('');
     const [latitude, setLatitude] = useState('');
     const [longitude, setLongitude] = useState('');
     const [breweries, setBreweries] = useState([]);
 
-    //creating function to load ip address from the API
+    //creating function to load ip address and get coordinates
     const getData = async () => {
         const res1 = await axios.get('https://api.ipify.org?format=json')
         setIP(res1.data.ip)
         const res2 = await axios.get(`http://ip-api.com/json/${ip}`)
         setLatitude(res2.data.lat)
         setLongitude(res2.data.lon)
-        console.log(latitude)
-        console.log(longitude)
-
     }
 
+    // Searches breweries by geo coordinates
     const searchBreweries = async () => {
         const res = await axios.get(`https://api.openbrewerydb.org/breweries?per_page=10&by_dist=${latitude},${longitude}`)
         setBreweries(res.data)
     }
 
-    // const searchBreweries = async () => {
-    //     const query = { latitude, longitude };
-    //     const response = await fetch('/geo', {
-    //         method: 'POST',
-    //         body: JSON.stringify(query),
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //     });
-    //     const data = await response.json();
-    //     setBreweries(data);
-    // }
-
     useEffect(() => {
-        //passing getData method to the lifecycle method
         getData()
         searchBreweries()
     }, [latitude, longitude])
 
-    
-
     return (
         <div>
             <Navbar />
-            <div className="HomePageText">
-                <h1>Home Page</h1>
-                <Link to='/breweries'>
-                    <Button variant="primary">Go to breweries</Button>
-                </Link>
-                <div>
-                    <h1>Breweries Close To You:</h1>
-                    <BreweryList breweries={breweries} />
-                </div>
 
+            <div>
+                <div class="row home-row">
+                    <div class="welcome-div col-4">
+                        <h1 class="header-text">HopHub</h1>
+                        <img class="icon-img" src={icon}></img>
+                        <br />
+                        <div class="page-button">
+                            <Link to='/breweries'>
+                                <Button variant="primary">Search By City</Button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div class="col-8 ">
+                        <h2>Breweries Close To You:</h2>
+                        <BreweryList breweries={breweries} />
+                    </div>
+                </div>
+            </div>
+            <div class="footer bg-primary">
+                <h3>© Our team 2022</h3>
             </div>
         </div>
     )
