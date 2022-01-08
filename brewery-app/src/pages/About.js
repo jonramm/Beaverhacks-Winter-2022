693954ad85524f2aa81639eececb8b5096d62006
Jon Ramm <jrgbone@gmail.com>
2022-01-08 14:59:34 -0700

    add boilerplate content in About page

--- brewery-app/src/pages/About.js
+++ brewery-app/src/pages/About.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Navbar from '../components/pageNavbar';
 
 function About() {
-
+    return (
+        <div className="page-container">
+            <Navbar />
+            <div className="content-wrap">
+    
+            </div>
+            <div className="footer bg-primary">
+                <h3 id="footerText">© Our team 2022</h3>
+            </div>
+        </div>
+    )
 }
 
 export default About;
