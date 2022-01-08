12a74a12b1f3f56f4b03aad0ad4bdf9b0493c5e4
Jon Ramm <jrgbone@gmail.com>
2022-01-08 15:59:38 -0700

    create about info boxes

--- brewery-app/src/pages/About.js
+++ brewery-app/src/pages/About.js
@@ -6,7 +6,23 @@ function About() {
         <div className="page-container">
             <Navbar />
             <div className="content-wrap">
-    
+                <h2>This site was built for the love of beer by:</h2>
+                <div className="container px-4">
+                <div className="row gx-5">
+                    <div className="col about-box-left p-3 border bg-light">
+                        <h3>Matthew DeMichele</h3>                 
+                    </div>
+                    <div className="col about-box-left p-3 border bg-light">
+                        <h3>Jon Ramm</h3>
+                    </div>
+                    <div className="col about-box-left p-3 border bg-light">
+                        <h3>Jonathan Rockett</h3>
+                    </div>
+                    <div className="col about-box-right p-3 border bg-light">
+                        <h3>Richard Silva</h3>
+                    </div>
+                </div>
+                </div>
             </div>
             <div className="footer bg-primary">
                 <h3 id="footerText">© Our team 2022</h3>
