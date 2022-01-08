6340d10ae9312b720ab5a908b2062a5e4bfdd30f
Jon Ramm <jrgbone@gmail.com>
2022-01-08 16:18:12 -0700

    about boxes have pictures, website links, and favorite style

--- brewery-app/src/pages/About.js
+++ brewery-app/src/pages/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from '../components/pageNavbar';
+import jonImg from '../images/github-prof-img.jpg'
+import beerStein from '../images/beer-stein.png';
 
 function About() {
     return (
@@ -10,16 +12,28 @@ function About() {
                 <div className="container px-4">
                 <div className="row gx-5">
                     <div className="col about-box-left p-3 border bg-light">
-                        <h3>Matthew DeMichele</h3>                 
+                        <h3><u>Matthew DeMichele</u></h3> 
+                        <img className="about-img" src={beerStein}></img>
+                        <p><a className="personal-site" href="" target="_blank">Visit my website!</a></p>
+                        <p>Favorite Style:</p>                
                     </div>
                     <div className="col about-box-left p-3 border bg-light">
-                        <h3>Jon Ramm</h3>
+                        <h3><u>Jon Ramm</u></h3>
+                        <img className="about-img" src={jonImg}></img>
+                        <p><a className="personal-site" href="jonramm.com" target="_blank">Visit my website!</a></p>
+                        <p>Favorite Style: IPA</p>
                     </div>
                     <div className="col about-box-left p-3 border bg-light">
-                        <h3>Jonathan Rockett</h3>
+                        <h3><u>Jonathan Rockett</u></h3>
+                        <img className="about-img" src={beerStein}></img> 
+                        <p><a className="personal-site" href="" target="_blank">Visit my website!</a></p>
+                        <p>Favorite Style:</p>  
                     </div>
                     <div className="col about-box-right p-3 border bg-light">
-                        <h3>Richard Silva</h3>
+                        <h3><u>Richard Silva</u></h3>
+                        <img className="about-img" src={beerStein}></img>
+                        <p><a className="personal-site" href="" target="_blank">Visit my website!</a></p>
+                        <p>Favorite Style:</p>   
                     </div>
                 </div>
                 </div>
